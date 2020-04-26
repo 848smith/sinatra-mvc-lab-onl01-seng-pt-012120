f55f0813ce801b9304901437d5225f1b48af9438
Andrew Smith <848smith@gmail.com>
2020-04-26 03:22:28 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,11 +10,16 @@ class PigLatinizer
         user_text + front + "ay"
       end
     else
-      
-  end
-  
-  def phrase_to_pig(phrase)
-    array = phrase.split(" ")
-    @pig_phrase = phrase.collect {|word| piglatinize(word)}.join(" ")
+      array = user_text.split(" ")
+      pig_phrase = user_text.collect |word| do
+        if user_text.downcase.index(/[aeiou]/) == 0
+          user_text + "way"
+        else
+          vowel_index = user_text.index(/[aeiou]/)
+          front = user_text.slice!(0..vowel_index-1)
+          user_text + front + "ay"
+        end
+      end.join(" ")
+    end
   end
 end
